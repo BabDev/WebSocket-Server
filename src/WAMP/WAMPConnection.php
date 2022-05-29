0e761919bece6f35a1835c50e23eec684030cd62
Michael Babker <michael.babker@gmail.com>
2022-05-29 18:29:26 -0500

    Add a WAMP connection decorator

---
+++ src/WAMP/WAMPConnection.php
@@ -0,0 +1,130 @@
+<?php declare(strict_types=1);
+
+namespace BabDev\WebSocket\Server\WAMP;
+
+use BabDev\WebSocket\Server\Connection;
+use BabDev\WebSocket\Server\Connection\AttributeStore;
+
+/**
+ * The WAMP connection is a connection class decorating another {@see Connection} adding helper methods to
+ * send WAMP messages to the connected client.
+ */
+final class WAMPConnection implements Connection
+{
+    private const CURIE_SEPARATOR = ':';
+
+    public function __construct(
+        private readonly Connection $connection,
+    ) {
+    }
+
+    public function getAttributeStore(): AttributeStore
+    {
+        return $this->connection->getAttributeStore();
+    }
+
+    public function getConnection(): Connection
+    {
+        return $this->connection;
+    }
+
+    public function send(string $data): void
+    {
+        $this->connection->send($data);
+    }
+
+    public function close(mixed $data = null): void
+    {
+        $this->connection->close($data);
+    }
+
+    /**
+     * Sends a "CALLRESULT" WAMP message to the client.
+     *
+     * @param string $id     The unique ID given by the client to respond to
+     * @param mixed  $result The call result, must be a JSON serializable value
+     *
+     * @throws \JsonException if the message cannot be JSON encoded
+     */
+    public function callResult(string $id, mixed $result = null): void
+    {
+        $this->send(json_encode([MessageType::CALL_RESULT, $id, $result], \JSON_THROW_ON_ERROR));
+    }
+
+    /**
+     * Sends a "CALLERROR" WAMP message to the client.
+     *
+     * @param string $id               The unique ID given by the client to respond to
+     * @param string $errorUri         The URI given to identify the error
+     * @param string $errorDescription An optional human-readable description of the error
+     * @param string $errorDetails     Used to communicate application error details defined by the error URI; if given, must be a JSON serializable value
+     *
+     * @throws \JsonException if the message cannot be JSON encoded
+     */
+    public function callError(string $id, string $errorUri, string $errorDescription = '', mixed $errorDetails = null): void
+    {
+        $data = [MessageType::CALL_ERROR, $id, $errorUri, $errorDescription];
+
+        if (null !== $errorDetails) {
+            $data[] = $errorDetails;
+        }
+
+        $this->send(json_encode($data, \JSON_THROW_ON_ERROR));
+    }
+
+    /**
+     * Sends a "EVENT" WAMP message to the client.
+     *
+     * @param string $topicUri The topic to broadcast to
+     * @param mixed  $event    Data to send with the event, must be a JSON serializable value
+     *
+     * @throws \JsonException if the message cannot be JSON encoded
+     */
+    public function event(string $topicUri, mixed $event): void
+    {
+        $this->send(json_encode([MessageType::EVENT, $topicUri, $event], \JSON_THROW_ON_ERROR));
+    }
+
+    /**
+     * Sends a "PREFIX" WAMP message to the client.
+     *
+     * @param string $prefix The string to use as the prefix
+     * @param string $uri    The URI which will be abbreviated with the given prefix
+     *
+     * @throws \JsonException if the message cannot be JSON encoded
+     */
+    public function prefix(string $prefix, string $uri): void
+    {
+        /** @var array<string, string> $prefixes */
+        $prefixes = $this->getAttributeStore()->get('wamp.prefixes', []);
+        $prefixes[$prefix] = $uri;
+
+        $this->getAttributeStore()->set('wamp.prefixes', $prefixes);
+
+        $this->send(json_encode([MessageType::PREFIX, $prefix, $uri], \JSON_THROW_ON_ERROR));
+    }
+
+    public function getUri(string $uri): string
+    {
+        $hasHttpProtocol = preg_match('/http(s*)\:\/\//', $uri);
+
+        if (false !== $hasHttpProtocol && 0 !== $hasHttpProtocol) {
+            return $uri;
+        }
+
+        if (!str_contains($uri, self::CURIE_SEPARATOR)) {
+            return $uri;
+        }
+
+        [$prefix, $action] = explode(self::CURIE_SEPARATOR, $uri);
+
+        /** @var array<string, string> $prefixes */
+        $prefixes = $this->getAttributeStore()->get('wamp.prefixes', []);
+
+        if (isset($prefixes[$prefix])) {
+            return $prefixes[$prefix].'#'.$action;
+        }
+
+        return $uri;
+    }
+}
---
+++ tests/WAMP/WAMPConnectionTest.php
@@ -0,0 +1,208 @@
+<?php declare(strict_types=1);
+
+namespace BabDev\WebSocket\Server\Tests\WAMP;
+
+use BabDev\WebSocket\Server\Connection;
+use BabDev\WebSocket\Server\Connection\AttributeStore;
+use BabDev\WebSocket\Server\WAMP\MessageType;
+use BabDev\WebSocket\Server\WAMP\WAMPConnection;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class WAMPConnectionTest extends TestCase
+{
+    private MockObject & Connection $decoratedConnection;
+
+    private WAMPConnection $connection;
+
+    protected function setUp(): void
+    {
+        $this->decoratedConnection = $this->createMock(Connection::class);
+
+        $this->connection = new WAMPConnection($this->decoratedConnection);
+    }
+
+    public function testProvidesTheAttributeStoreFromTheDecoratedConnection(): void
+    {
+        /** @var MockObject&AttributeStore $attributeStore */
+        $attributeStore = $this->createMock(AttributeStore::class);
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('getAttributeStore')
+            ->willReturn($attributeStore);
+
+        $this->assertSame($attributeStore, $this->connection->getAttributeStore());
+    }
+
+    public function testProvidesTheDecoratedConnection(): void
+    {
+        $this->assertSame($this->decoratedConnection, $this->connection->getConnection());
+    }
+
+    public function testSendsAMessage(): void
+    {
+        $message = 'Testing';
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('send')
+            ->with($message);
+
+        $this->connection->send('Testing');
+    }
+
+    public function testClosesAConnection(): void
+    {
+        $data = 'Close';
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('close')
+            ->with($data);
+
+        $this->connection->close($data);
+    }
+
+    public function testSendsACallResultMessage(): void
+    {
+        $callId = uniqid();
+        $data = ['hello' => 'world'];
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('send')
+            ->with(json_encode([MessageType::CALL_RESULT, $callId, $data]));
+
+        $this->connection->callResult($callId, $data);
+    }
+
+    public function testSendsACallErrorMessage(): void
+    {
+        $callId = uniqid();
+        $uri = 'https://example.com/error#testing';
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('send')
+            ->with(json_encode([MessageType::CALL_ERROR, $callId, $uri, 'Testing Error']));
+
+        $this->connection->callError($callId, $uri, 'Testing Error');
+    }
+
+    public function testSendsACallErrorMessageWithDetails(): void
+    {
+        $callId = uniqid();
+        $uri = 'https://example.com/error#testing';
+        $details = 'Testing';
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('send')
+            ->with(json_encode([MessageType::CALL_ERROR, $callId, $uri, 'Testing Error', $details]));
+
+        $this->connection->callError($callId, $uri, 'Testing Error', $details);
+    }
+
+    public function testSendsAnEventMessage(): void
+    {
+        $uri = 'https://example.com/testing';
+        $data = ['hello' => 'world'];
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('send')
+            ->with(json_encode([MessageType::EVENT, $uri, $data]));
+
+        $this->connection->event($uri, $data);
+    }
+
+    public function testSendsAPrefixMessage(): void
+    {
+        $prefix = 'testing';
+        $uri = 'https://example.com/testing';
+
+        /** @var MockObject&AttributeStore $attributeStore */
+        $attributeStore = $this->createMock(AttributeStore::class);
+        $attributeStore->expects($this->once())
+            ->method('get')
+            ->with('wamp.prefixes', [])
+            ->willReturn([]);
+
+        $attributeStore->expects($this->once())
+            ->method('set')
+            ->with('wamp.prefixes', [$prefix => $uri]);
+
+        $this->decoratedConnection->expects($this->atLeastOnce())
+            ->method('getAttributeStore')
+            ->willReturn($attributeStore);
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('send')
+            ->with(json_encode([MessageType::PREFIX, $prefix, $uri]));
+
+        $this->connection->prefix($prefix, $uri);
+    }
+
+    public function testGetUriWithNonHttpProtocol(): void
+    {
+        $uri = 'ssh://example.com/testing';
+
+        /** @var MockObject&AttributeStore $attributeStore */
+        $attributeStore = $this->createMock(AttributeStore::class);
+        $attributeStore->expects($this->once())
+            ->method('get')
+            ->with('wamp.prefixes', [])
+            ->willReturn([]);
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('getAttributeStore')
+            ->willReturn($attributeStore);
+
+        $this->assertSame($uri, $this->connection->getUri($uri));
+    }
+
+    public function testGetUriWithoutCurieSeparator(): void
+    {
+        $uri = 'example.com/testing';
+
+        $this->assertSame($uri, $this->connection->getUri($uri));
+    }
+
+    public function testGetUriWithHttpProtocol(): void
+    {
+        $uri = 'https://example.com/testing';
+
+        $this->assertSame($uri, $this->connection->getUri($uri));
+    }
+
+    public function testGetUriWithCurieAndUnregisteredPrefix(): void
+    {
+        $uri = 'testing:curie';
+
+        /** @var MockObject&AttributeStore $attributeStore */
+        $attributeStore = $this->createMock(AttributeStore::class);
+        $attributeStore->expects($this->once())
+            ->method('get')
+            ->with('wamp.prefixes', [])
+            ->willReturn([]);
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('getAttributeStore')
+            ->willReturn($attributeStore);
+
+        $this->assertSame($uri, $this->connection->getUri($uri));
+    }
+
+    public function testGetUriWithCurieAndRegisteredPrefix(): void
+    {
+        $prefix = 'testing';
+        $uri = 'https://example.com/testing';
+
+        /** @var MockObject&AttributeStore $attributeStore */
+        $attributeStore = $this->createMock(AttributeStore::class);
+        $attributeStore->expects($this->once())
+            ->method('get')
+            ->with('wamp.prefixes', [])
+            ->willReturn([$prefix => $uri]);
+
+        $this->decoratedConnection->expects($this->once())
+            ->method('getAttributeStore')
+            ->willReturn($attributeStore);
+
+        $this->assertSame($uri.'#curie', $this->connection->getUri('testing:curie'));
+    }
+}
