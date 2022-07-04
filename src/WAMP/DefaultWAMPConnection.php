84c104743bce06c3004c50adfaae477052bc9cec
Michael Babker <michael.babker@gmail.com>
2022-07-03 19:56:08 -0500

    Update PHPStan and refresh baseline

--- src/WAMP/DefaultWAMPConnection.php
+++ src/WAMP/DefaultWAMPConnection.php
@@ -36,7 +36,7 @@ final class DefaultWAMPConnection implements WAMPConnection
     public function send(string|DataInterface $data): void
     {
         if ($data instanceof DataInterface && !$this->connection instanceof WebSocketConnection) {
-            throw new UnsupportedConnection(sprintf('To send a %s implementation, the "%s" class must decorate an instance of "%s".', DataInterface::class, self::class, WebSocketConnection::class));
+            throw new UnsupportedConnection(sprintf('To send a "%s" implementation, "%s" must decorate an instance of "%s".', DataInterface::class, self::class, WebSocketConnection::class));
         }
 
         $this->connection->send($data);
