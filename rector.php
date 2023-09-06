837e6f7a0a95acc17e2d393f6ac7377dfac9e902
Michael Babker <michael@happydog.digital>
2023-09-06 10:11:00 -0400

    Bump dev tools

--- rector.php
+++ rector.php
@@ -2,9 +2,8 @@
 
 use Rector\Config\RectorConfig;
 use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
-use Rector\PHPUnit\Rector\Class_\AddSeeTestAnnotationRector;
+use Rector\PHPUnit\CodeQuality\Rector\Class_\AddSeeTestAnnotationRector;
 use Rector\PHPUnit\Set\PHPUnitLevelSetList;
-use Rector\PHPUnit\Set\PHPUnitSetList;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 
@@ -25,15 +24,14 @@ return static function (RectorConfig $rectorConfig): void {
     // AddSeeTestAnnotationRector
     $rectorConfig->importNames();
     $rectorConfig->importShortClasses(false);
-    $rectorConfig->phpstanConfig(__DIR__ . '/phpstan.neon');
+    $rectorConfig->phpstanConfigs([
+        __DIR__ . '/vendor/phpstan/phpstan-phpunit/extension.neon',
+        __DIR__ . '/phpstan.neon',
+    ]);
 
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_81,
         SetList::CODE_QUALITY,
         PHPUnitLevelSetList::UP_TO_PHPUNIT_100,
-        PHPUnitSetList::PHPUNIT_CODE_QUALITY,
-        PHPUnitSetList::PHPUNIT_EXCEPTION,
-        PHPUnitSetList::PHPUNIT_YIELD_DATA_PROVIDER,
-        PHPUnitSetList::REMOVE_MOCKS,
     ]);
 };
