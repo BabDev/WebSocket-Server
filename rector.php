ec15ace91569f8299e2f4110db49c1f33eeb0a95
Michael Babker <michael@happydog.digital>
2026-05-29 08:28:27 -0400

    Refine configs

--- rector.php
+++ rector.php
@@ -1,5 +1,6 @@
 <?php declare(strict_types=1);
 
+use Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector;
 use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
@@ -7,11 +8,15 @@ return RectorConfig::configure()
         __DIR__.'/src',
         __DIR__.'/tests',
     ])
+    ->withSkip([
+        ThrowWithPreviousExceptionRector::class, // Forces re-throwing with the wrapped exception's code, we aren't interested in this
+    ])
     ->withImportNames(importShortClasses: false)
     ->withPHPStanConfigs([
         __DIR__ . '/vendor/phpstan/phpstan-phpunit/extension.neon',
         __DIR__ . '/phpstan.neon',
     ])
+    ->withComposerBased(phpunit: true)
     ->withPhpSets()
     ->withPreparedSets(codeQuality: true, phpunitCodeQuality: true)
 ;
